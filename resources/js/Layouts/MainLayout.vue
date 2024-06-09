cffa5b0c4fe227d8f6efddb582a820d91a68dec3
Samuel de Rougemont <sderougemont@myprivateboutique.ch>
2024-06-09 19:40:35 +0200

    init inertia+vue3

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <header>
+      <h1>MAIN LAYOUT HEADER</h1>
+      <Link href="/">Home</Link> &nbsp;
+      <Link href="/hello">hello</Link>
+      <!-- <Link href="/contact">Contact</Link> -->
+    </header>
+    <article>
+      <slot />
+    </article>
+  </main>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+// import { ref } from 'vue'
+
+// const counter = ref(0)
+// setInterval(() => counter.value++, 1000)
+
+</script>
---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div class="">
+    Hola - The message is {{ message }} from the app
+  </div>
+
+</template>
+
+<script setup>
+
+import { ref } from 'vue'
+import { Link } from '@inertiajs/vue3'
+
+const props = defineProps({
+  message: String,
+  // someProp: {
+  //   type: String,
+  //   default: 'De Fault',
+  // }
+});
+</script>
+
+<script>
+// import MainLayout from '@/Layouts/MainLayout.vue'
+export default {
+  // layout: MainLayout,
+}
+</script>
